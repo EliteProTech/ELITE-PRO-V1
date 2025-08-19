8f66f93001f98cac8637838ed59668dde07dc2c0
ayoubrajol1 <ayoubrajol928@gmail.com>
2025-08-19 23:04:28 +0100

    Update Config.js

--- Config.js
+++ Config.js
@@ -28,7 +28,7 @@ global.autoreact = process.env.AUTO_REACT === 'true';
 global.autolikestatus = process.env.AUTO_LIKE_STATUS === 'true';
 
 // Thumbnail profile picture
-global.elitepropp = process.env.ELITE_PRO_PP || 'https://i.ibb.co/jk0ynvbn/7a8c4b5b617fa11a1e9a61190f427546.jpg';
+global.elitepropp = process.env.ELITE_PRO_PP || 'https://i.ibb.co/mr55V27D/buro.jpg';
 // Default emoji
 global.themeemoji = process.env.THEME_EMOJI || '👨‍💻';
 
