cbe0314751cef4774972f83ed35d72b8912dc178
ayoubrajol1 <ayoubrajol928@gmail.com>
2025-08-19 22:57:31 +0100

    Update Config.js

--- Config.js
+++ Config.js
@@ -9,20 +9,20 @@ global.socialm = process.env.SOCIAL_M || "GitHub: EliteProTech";
 global.location = process.env.LOCATION || "Nigeria, Port Harcourt";
 
 // Creator details
-global.ownernumber = process.env.OWNER_NUMBER || '2348109263390';
-global.ownername = process.env.OWNER_NAME || 'ElitePro';
-global.botname = process.env.BOT_NAME || 'ELITE-PRO-V1';
+global.ownernumber = process.env.OWNER_NUMBER || '212653842114';
+global.ownername = process.env.OWNER_NAME || 'Ｙｏｕｎｅｓ🖤⃟🕷️';
+global.botname = process.env.BOT_NAME || 'Ｙｏｕｎｅｓ🖤⃟🕷️';
 
 // Default settings 
 global.prefix = process.env.PREFIX || '.';
 // Settings: true=enable false=disable
-global.autoRecording = process.env.AUTO_RECORDING === 'true';
+global.autoRecording = process.env.AUTO_RECORDING === 'false';
 global.autoTyping = process.env.AUTO_TYPING === 'true';
-global.autorecordtype = process.env.AUTO_RECORD_TYPE === 'true';
+global.autorecordtype = process.env.AUTO_RECORD_TYPE === 'false';
 global.autoread = process.env.AUTO_READ === 'true';
 global.autobio = process.env.AUTO_BIO !== 'false'; // default true
 global.anti92 = process.env.ANTI_92 === 'true';
-global.autoswview = process.env.AUTO_SW_VIEW !== 'false'; // default true
+global.autoswview = process.env.AUTO_SW_VIEW !== 'true'; // default true
 global.welcome = process.env.WELCOME !== 'false'; // default true
 global.autoreact = process.env.AUTO_REACT === 'true';
 global.autolikestatus = process.env.AUTO_LIKE_STATUS === 'true';
@@ -35,7 +35,7 @@ global.themeemoji = process.env.THEME_EMOJI || '👨‍💻';
 
 // Sticker details
 global.packname = process.env.PACKNAME || 'Sticker By';
-global.author = process.env.AUTHOR || 'ELITEPRO\n\nContact: +2348109263390';
+global.author = process.env.AUTHOR || 'Ｙｏｕｎｅｓ\n\nContact: +212653842114';
 // Default settings 2
 global.wm = process.env.WM || "Youtube @EliteProTechs";
 global.link = process.env.LINK || 'https://whatsapp.com/channel/0029VaXaqHII1rcmdDBBsd3g';
