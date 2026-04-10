eecc459f02327c83ddba01418ab977dd30dc8df3
EliteProTech <proelite455@gmail.com>
2026-04-10 19:16:55 +0100

    EliteProTech

--- config.js
+++ config.js
@@ -20,10 +20,10 @@ global.autoRecording = process.env.AUTO_RECORDING === 'true';
 global.autoTyping = process.env.AUTO_TYPING === 'true';
 global.autorecordtype = process.env.AUTO_RECORD_TYPE === 'true';
 global.autoread = process.env.AUTO_READ === 'true';
-global.autobio = process.env.AUTO_BIO !== 'false'; // default true
+global.autobio = process.env.AUTO_BIO !== 'false';
 global.anti92 = process.env.ANTI_92 === 'true';
-global.autoswview = process.env.AUTO_SW_VIEW !== 'false'; // default true
-global.welcome = process.env.WELCOME !== 'false'; // default true
+global.autoviewstatus = process.env.AUTO_VIEW_STATUS !== 'false';
+global.welcome = process.env.WELCOME !== 'false';
 global.autoreact = process.env.AUTO_REACT === 'true';
 global.autolikestatus = process.env.AUTO_LIKE_STATUS === 'true';
 
@@ -33,7 +33,7 @@ global.themeemoji = process.env.THEME_EMOJI || '👨‍💻';
 
 // Sticker details
 global.packname = process.env.PACKNAME || 'Sticker By';
-global.author = process.env.AUTHOR || 'ELITEPRO\n\nContact: +2347047504860';
+global.author = process.env.AUTHOR || 'EliteProTech\n\nContact: +2347047504860';
 // Default settings 2
 global.wm = process.env.WM || "Youtube @EliteProTechs";
 global.link = process.env.LINK || 'https://whatsapp.com/channel/0029VaXaqHII1rcmdDBBsd3g';
